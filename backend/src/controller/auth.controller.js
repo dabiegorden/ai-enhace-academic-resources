c38fb87736af0b3fc15517e2b20b261eadab4200
DABIE GORDEN <117079883+dabiegorden@users.noreply.github.com>
2026-01-12 15:23:13 +0000

    commit

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcryptjs";
-import User from "../models/User.model.js";
+import User from "../models/user.model.js";
 import { generateToken } from "../utils/generateToken.js";
 
 // @desc    Register new user
